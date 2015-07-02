b501df36390129bd8b5ef2f7b9eeb227a1865c76
54youngliang@gmail.com <54youngliang@gmail.com>
2015-07-02 12:41:10 +0800

    commit

--- Bubble_Client/Assets/Scripts/LevelWindow.cs
+++ Bubble_Client/Assets/Scripts/LevelWindow.cs
@@ -4,42 +4,44 @@ using System.Collections.Generic;
 
 public class LevelWindow : MonoBehaviour {
 
-	public UIScrollView scrollView;
+	//public UIScrollView scrollView;
 	public GameObject levelItemPrefab;
 	public Transform grid;
-	public GameObject levelBgPrefab;
+	//public GameObject levelBgPrefab;
 	public GameObject background;
 
 	private List<LevelView> viewList;
 	// Use this for initialization
 	void Start () {
+		var width = AppMain.Instance.uiRoot.manualWidth;
+		this.transform.localPosition = new Vector3(width, 0, 0);
 		Init ();
 	}
 	
 	private void Init()
 	{
-		viewList = new List<LevelView>();
-		for(int i = 0; i < 100; i ++)
-		{
-			var go = GameObjectUtil.CloneGameObject(levelItemPrefab, scrollView.transform);
-			var item = go.GetComponent<LevelView>();
-			viewList.Add(item);
-			int x = Random.Range(-300, 300);
-			item.transform.localPosition = new Vector3(x, i * 200 - 360, 0);
-			item.Level = i + 1;
-			item.Star = PlayerPrefs.GetInt("star_level_" + i, -1);
-			item.OnItemClick = OnLevelClick;
-		}
-
-		float maxY = viewList[viewList.Count - 1].transform.localPosition.y;
-		int ah = AppMain.Instance.uiRoot.activeHeight;
-		int count = (int)Mathf.Ceil(maxY / ah);
-		for(int i = 0; i < count; i ++)
-		{
-			GameObjectUtil.CloneGameObject(levelBgPrefab, grid);
-		}
-		UIGrid uiGrid = grid.GetComponent<UIGrid>();
-		uiGrid.Reposition();
+//		viewList = new List<LevelView>();
+//		for(int i = 0; i < 100; i ++)
+//		{
+//			//var go = GameObjectUtil.CloneGameObject(levelItemPrefab, scrollView.transform);
+//			var item = go.GetComponent<LevelView>();
+//			viewList.Add(item);
+//			int x = Random.Range(-300, 300);
+//			item.transform.localPosition = new Vector3(x, i * 200 - 360, 0);
+//			item.Level = i + 1;
+//			item.Star = PlayerPrefs.GetInt("star_level_" + i, -1);
+//			item.OnItemClick = OnLevelClick;
+//		}
+//
+//		float maxY = viewList[viewList.Count - 1].transform.localPosition.y;
+//		int ah = AppMain.Instance.uiRoot.activeHeight;
+//		int count = (int)Mathf.Ceil(maxY / ah);
+//		for(int i = 0; i < count; i ++)
+//		{
+//		//	GameObjectUtil.CloneGameObject(levelBgPrefab, grid);
+//		}
+//		UIGrid uiGrid = grid.GetComponent<UIGrid>();
+//		uiGrid.Reposition();
 	}
 
 	public void ShowHome()
